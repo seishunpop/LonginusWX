3dc8d7914152f80b5aacabb7eb1cfba04f57a437
unknown <exalted527@gmail.com>
2023-04-25 21:33:22 -0700

    Working on weather

--- taf.py
+++ taf.py
@@ -5,7 +5,7 @@ import pandas as pd
 # Constructs query for selected stations
 
 query_string = "https://www.aviationweather.gov/adds/dataserver_current/httpparam?datasource=tafs&requestType=retrieve&format=xml&mostRecentForEachStation=true&hoursBeforeNow=2&stationString="
-station = "kely"
+station = "kdys"
 
 # Requests TAF to instantiate bs4 then finds all TAF lines
 # TAF data is requested from the Aviation Weather Center API
@@ -36,13 +36,18 @@ for x in taf_lines:
     wind_gust_kt = None
     if x.wind_gust_kt:
         wind_gust_kt = x.wind_gust_kt.string
-    visibility_statute_mi = x.visibility_statute_mi.string
+    visibility_statute_mi = None
+    if x.visibility_statute_mi:
+        visibility_statute_mi = x.visibility_statute_mi.string
     wx_string = "NSW"
     if x.wx_string:
         wx_string = x.wx_string.string
     sky_condition = {"cloud_base_ft_agl": [], "sky_cover": []}
     for z in x.select("forecast > sky_condition"):
-        sky_condition["cloud_base_ft_agl"].append(z["cloud_base_ft_agl"])
+        try:
+            sky_condition["cloud_base_ft_agl"].append(z["cloud_base_ft_agl"])
+        except:
+            sky_condition["cloud_base_ft_agl"].append(None)
         sky_condition["sky_cover"].append(z["sky_cover"])
 
     taf["fcst_from"].append(fcst_time_from)
@@ -55,10 +60,11 @@ for x in taf_lines:
     taf["sky_con"].append(sky_condition)
 
 # Converts TAF to dataframe then drops lines outside of valid times
+
 df = pd.DataFrame(taf)
 
-valid_from = "2023-04-24T06:00:00Z"
-valid_to = "2023-04-24T17:00:00Z"
+valid_from = "2023-04-26T16:00:00Z"
+valid_to = "2023-04-26T20:00:00Z"
 df.drop(df[df.fcst_to < valid_from].index, inplace=True)
 df.drop(df[df.fcst_from >= valid_to].index, inplace=True)
 
@@ -73,4 +79,22 @@ forecast = {
     "wnd_dir": None,
 }
 
-print(df)
+if df.wnd_gust.any():
+    gust = df["wnd_gust"].max()
+    winds = df.loc[df["wnd_gust"] == gust].iloc[0]
+    forecast["wnd_dir"] = winds.wnd_dir
+    forecast["wnd_speed"] = winds.wnd_speed
+    forecast["wnd_gust"] = winds.wnd_gust
+else:
+    speed = df["wnd_speed"].max()
+    winds = df.loc[df["wnd_speed"] == speed].iloc[0]
+    forecast["wnd_dir"] = winds.wnd_dir
+    forecast["wnd_speed"] = winds.wnd_speed
+
+vis = df["visibility"].min()
+if vis == "6.21":
+    forecast["visibility"] = "7"
+else:
+    forecast["visibility"] = vis
+
+print(forecast)
